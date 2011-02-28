5f7b9d608f8b6c6f93803f6f839e35669e37f83f
Kevin Branigan <kbranigan@gmail.com>
2011-02-28 11:58:31 -0500

    updated read_shapefile to support passing a specific id, also it generates GL_POINTS, GL_LINE_STRIP and GL_LINE_LOOP from SHPT_POINT, SHPT_ARC and SHPT_POLYGON respectively.

--- read_shapefile.c
+++ read_shapefile.c
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
   char * filename = (argc > 1) ? argv[1] : "prov_ab_p_geo83_e.dbf";
+  int id          = (argc > 2) ? atoi(argv[2]) : -1;
   
   FILE * fp = fopen(filename, "r");
   if (!fp)
@@ -39,36 +40,19 @@ int main(int argc, char *argv[])
   long i;
   for (i = 0 ; i < nRecordCount ; i++)
   {
+    if (id != -1 && id != i) continue;
     SHPObject	*psShape = SHPReadObject(h, i);
-    int j, iPart;
-    if (psShape->nSHPType != SHPT_POLYGON) fprintf(stderr, "doesn't do nSHPType:%d yet.\n", psShape->nSHPType);
-    
-    //if (psShape->nShapeId != 2)
-    //  continue;
-    
-    //fprintf(stderr, "shape %d has %d parts and %d vertices\n", psShape->nShapeId, psShape->nParts, psShape->nVertices);
-    
-    //if (psShape->nParts > 1)
-    //  psShape->nVertices = psShape->panPartStart[1];
-    
-    for (iPart = 0 ; iPart < psShape->nParts ; iPart++)
+    int j;
+    if (psShape->nSHPType == SHPT_POINT)
     {
-      int start = psShape->panPartStart[iPart];
-      int end = psShape->nVertices;
-      
-      if (iPart != psShape->nParts - 1)
-      {
-        end = psShape->panPartStart[iPart+1];
-      }
-      
       struct Shape * shape = (struct Shape*)malloc(sizeof(struct Shape));
       memset(shape, 0, sizeof(struct Shape));
       shape->unique_set_id = t++;
-      shape->gl_type = GL_LINE_LOOP;
-      shape->num_vertexs = end - start;
-      shape->num_attributes = 0;
+      shape->gl_type = GL_POINTS;
       
-      //fprintf(stderr, "%d: %d %d (%d)\n", iPart, start, end, shape->num_vertexs);
+      shape->num_vertexs = psShape->nVertices;
+      if (shape->num_vertexs <= 0) { fprintf(stderr, "shape->num_vertexs = %d", shape->num_vertexs); return 0; }
+      shape->num_attributes = 0;
       
       shape->num_vertex_arrays = 1;
       shape->vertex_arrays = (struct VertexArray*)malloc(sizeof(struct VertexArray)*shape->num_vertex_arrays);
@@ -78,20 +62,70 @@ int main(int argc, char *argv[])
       va->array_type = GL_VERTEX_ARRAY;
       va->vertexs = (double*)malloc(sizeof(double)*shape->num_vertexs*va->num_dimensions);
       //fprintf(stderr, "%ld: %d\n", i, psShape->nSHPType);
-      for (j = start ; j < end ; j++)
+      for (j = 0 ; j < shape->num_vertexs ; j++)
       {
-        va->vertexs[(j-start)*va->num_dimensions+0] = psShape->padfX[j];
-        va->vertexs[(j-start)*va->num_dimensions+1] = psShape->padfY[j];
+        va->vertexs[j*va->num_dimensions+0] = psShape->padfX[j];
+        va->vertexs[j*va->num_dimensions+1] = psShape->padfY[j];
       }
       write_shape(stdout, shape);
       free_shape(shape);
-      //if (iPart > 1) break;
-      //break;
     }
-    
-    //SHPDestroyObject(psShape);
-    //break;
-    //if (i > 1) break;
+    else if (psShape->nSHPType == SHPT_ARC || psShape->nSHPType == SHPT_POLYGON)
+    {
+      //fprintf(stderr, "shape %d has %d parts and %d vertices\n", psShape->nShapeId, psShape->nParts, psShape->nVertices);
+      
+      if (psShape->nParts == 0) { fprintf(stderr, "polygon %d has 0 parts (thats bad)\n", psShape->nShapeId); return 0; }
+      
+      int j, iPart;
+      for (iPart = 0 ; iPart < psShape->nParts ; iPart++)
+      {
+        int start = psShape->panPartStart[iPart];
+        int end = psShape->nVertices;
+        
+        if (iPart != psShape->nParts - 1)
+        {
+          end = psShape->panPartStart[iPart+1];
+        }
+        
+        struct Shape * shape = (struct Shape*)malloc(sizeof(struct Shape));
+        memset(shape, 0, sizeof(struct Shape));
+        shape->unique_set_id = t++;
+        if (psShape->nSHPType == SHPT_ARC)
+          shape->gl_type = GL_LINE_STRIP;
+        else if (psShape->nSHPType == SHPT_POLYGON)
+          shape->gl_type = GL_LINE_LOOP;
+        
+        shape->num_vertexs = end - start;
+        if (shape->num_vertexs <= 0) { fprintf(stderr, "shape->num_vertexs = %d", shape->num_vertexs); return 0; }
+        shape->num_attributes = 0;
+        
+        //fprintf(stderr, "%d: %d %d (%d)\n", iPart, start, end, shape->num_vertexs);
+        
+        shape->num_vertex_arrays = 1;
+        shape->vertex_arrays = (struct VertexArray*)malloc(sizeof(struct VertexArray)*shape->num_vertex_arrays);
+        
+        struct VertexArray *va = &shape->vertex_arrays[0];
+        va->num_dimensions = 2;
+        va->array_type = GL_VERTEX_ARRAY;
+        va->vertexs = (double*)malloc(sizeof(double)*shape->num_vertexs*va->num_dimensions);
+        //fprintf(stderr, "%ld: %d\n", i, psShape->nSHPType);
+        for (j = start ; j < end ; j++)
+        {
+          va->vertexs[(j-start)*va->num_dimensions+0] = psShape->padfX[j];
+          va->vertexs[(j-start)*va->num_dimensions+1] = psShape->padfY[j];
+        }
+        write_shape(stdout, shape);
+        free_shape(shape);
+        //if (iPart > 1) break;
+        break;
+      }
+    }
+    else
+    {
+      fprintf(stderr, "doesn't do nSHPType:%d yet.\n", psShape->nSHPType);
+      return 0;
+    }
+    SHPDestroyObject(psShape);
   }
   DBFClose(d);
   SHPClose(h);
