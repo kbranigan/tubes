78862b0e10348bf6d6277d91bf17817a7b1ec80c
Kevin Branigan <kbranigan@gmail.com>
2011-03-07 02:30:54 -0500

    removed the timeout for read_shape

--- scheme.c
+++ scheme.c
@@ -29,17 +29,18 @@ int stdout_is_piped()
 int stdin_has_data()
 {
   fd_set rfds;
-  struct timeval tv;
+  //struct timeval tv;
   int retval;
   
   FD_ZERO(&rfds);
   FD_SET(0, &rfds);
-  tv.tv_sec = 5;
-  tv.tv_usec = 0;
+  //tv.tv_sec = 5;
+  //tv.tv_usec = 0;
   
   if (feof(stdin)) return 0;
   
-  retval = select(1, &rfds, NULL, NULL, &tv);
+  //retval = select(1, &rfds, NULL, NULL, &tv);
+  retval = select(1, &rfds, NULL, NULL, NULL);
   
   if (retval == -1) { fprintf(stderr, "select() on stdin failed.\n"); exit(1); }
   return (retval != 0);
