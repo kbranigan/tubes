8163416d32638da927835bef9807d0101589fc38
Kevin Branigan <kbranigan@gmail.com>
2011-04-20 02:42:03 -0400

    update clip script to support difference, exclusive-or and union (in addition to the default intersection)

--- clip.c
+++ clip.c
@@ -13,22 +13,35 @@
 
 int clip(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, FILE * pipe_err)
 {
+  gpc_op gpc_operation = GPC_INT; // default is intersection
+  
   char * clipping_filename = NULL;
   int c;
-  while ((c = getopt(argc, argv, "c:")) != -1)
+  while ((c = getopt(argc, argv, "f:o:")) != -1)
   switch (c)
   {
-    case 'c':
+    case 'f':
       clipping_filename = malloc(strlen(optarg)+1);
       strcpy(clipping_filename, optarg);
+      break;
+    case 'o':
+      if (strcmp(optarg, "difference")==0)        gpc_operation = GPC_DIFF;
+      else if (strcmp(optarg, "intersection")==0) gpc_operation = GPC_INT;
+      else if (strcmp(optarg, "exclusive-or")==0) gpc_operation = GPC_XOR;
+      else if (strcmp(optarg, "union")==0)        gpc_operation = GPC_UNION;
+      else {
+        fprintf(pipe_err, "-o [difference|intersection|exclusive-or|union] ('%s' is invalid)\n", optarg);
+        exit(1);
+      }
       break;
     default:
       abort();
   }
+  
   if (clipping_filename == NULL) { fprintf(pipe_err, "-c [clipping_filename] is required\n"); exit(1); }
   
   FILE * clip_fp = fopen(clipping_filename, "r");
-  if (clip_fp == NULL) { fprintf(pipe_err, "-c [clipping_filename] is required (%s doesn't exist)\n", clipping_filename); exit(1); }
+  if (clip_fp == NULL) { fprintf(pipe_err, "-c [clipping_filename] is required ('%s' doesn't exist)\n", clipping_filename); exit(1); }
   
   gpc_polygon * clip_polygon = malloc(sizeof(gpc_polygon));
   clip_polygon->num_contours = 0;
@@ -83,21 +96,25 @@ int clip(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, FILE * pipe_er
     result_polygon->contour = NULL;
     result_polygon->hole = NULL;
     
-    gpc_polygon_clip(GPC_INT, org, clip_polygon, result_polygon);
+    gpc_polygon_clip(gpc_operation, org, clip_polygon, result_polygon);
     
-    free_shape(shape);
-    shape = new_shape();
-    shape->gl_type = GL_LINE_LOOP;
-    for (i = 0 ; i < result_polygon->contour[0].num_vertices ; i++)
+    int j;
+    for (j = 0 ; j < result_polygon->num_contours ; j++)
     {
-      float v[3] = {result_polygon->contour[0].vertex[i].x, result_polygon->contour[0].vertex[i].y, 0.0};
-      append_vertex(shape, v);
+      free_shape(shape);
+      shape = new_shape();
+      shape->gl_type = GL_LINE_LOOP;
+      for (i = 0 ; i < result_polygon->contour[j].num_vertices ; i++)
+      {
+        float v[3] = { result_polygon->contour[j].vertex[i].x, result_polygon->contour[j].vertex[i].y, 0.0 };
+        append_vertex(shape, v);
+      }
+      write_shape(pipe_out, shape);
     }
+    free_shape(shape);
     
     gpc_free_polygon(org);
     gpc_free_polygon(result_polygon);
-    
-    write_shape(pipe_out, shape);
   }
   
   gpc_free_polygon(clip_polygon);
