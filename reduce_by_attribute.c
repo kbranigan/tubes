378303d5ce5d215eb2750e542b950fa3adc8f4e5
Kevin Branigan <kbranigan@gmail.com>
2011-04-20 03:36:00 -0400

    fixed reduce_by_attribute

--- reduce_by_attribute.c
+++ reduce_by_attribute.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define SCHEME_CREATE_MAIN
 #define SCHEME_ASSERT_STDINOUT_ARE_PIPED
@@ -9,31 +11,28 @@
 
 int reduce_by_attribute(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, FILE * pipe_err)
 {
-  char * name = argc > 1 ? argv[1] : "";
-  char * value = argc > 2 ? argv[2] : "";
-  
-  long shapes_read = 0;
-  long shapes_write = 0;
-  
+  char name[21] = "";
+  char value[200] = "";
+  int c;
+  while ((c = getopt(argc, argv, "n:v:")) != -1)
+  switch (c)
+  {
+    case 'n':
+      strncpy(name, optarg, 20);
+      break;
+    case 'v':
+      strncpy(value, optarg, 20);
+      break;
+    default:
+      abort();
+  }
   struct Shape * shape = NULL;
   while ((shape = read_shape(pipe_in)))
   {
-    shapes_read++;
     if (shape->num_attributes == 0) continue;
+    if (strcmp(get_attribute(shape, name), value)!=0) continue;
     
-    int i;
-    for (i = 0 ; i < shape->num_attributes ; i++)
-    {
-      struct Attribute * attribute = &shape->attributes[i];
-      if (strcmp(attribute->name, name)==0 && strcmp(attribute->value, value)==0)
-      {
-        shapes_write++;
-        write_shape(pipe_out, shape);
-      }
-    }
-    
+    write_shape(pipe_out, shape);
     free_shape(shape);
   }
-  
-  fprintf(stderr, "%s: %ld shapes reduced to %ld\n", argv[0], shapes_read, shapes_write);
 }
