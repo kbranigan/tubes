1aa29455c1f5bf09e1b63ac2bcb5b97e51fdea34
Kevin Branigan <kbranigan@gmail.com>
2011-04-21 01:23:43 -0400

    fixed add_random_colors

--- add_random_colors.c
+++ add_random_colors.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define SCHEME_CREATE_MAIN
 #define SCHEME_ASSERT_STDINOUT_ARE_PIPED
@@ -17,9 +18,26 @@ void set_random_color(float * v)
 
 int add_random_colors(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, FILE * pipe_err)
 {
-  int per_shape_or_vertex = argc < 2 ? 0 : atoi(argv[1]);
-  int num_dimensions      = argc < 3 ? 4 : atoi(argv[2]);
-  if (num_dimensions != 3 && num_dimensions != 4) { fprintf(pipe_err, "num_dimensions (%d) is invalid, defaulting to 4\n", num_dimensions); num_dimensions = 4; }
+  int num_dimensions = 4;
+  int per_shape_or_vertex = 0;
+  int c;
+  while ((c = getopt(argc, argv, "d:p:")) != -1)
+  switch (c)
+  {
+    case 'd':
+      num_dimensions = clamp_int(atoi(optarg), 3, 4);
+      break;
+    case 'p':
+      if (strcmp(optarg, "shape")==0) per_shape_or_vertex = 0;
+      else if (strcmp(optarg, "vertex")==0) per_shape_or_vertex = 1;
+      else {
+        fprintf(pipe_err, "-p [shape|vertex] ('%s' is invalid)\n", optarg);
+        exit(1);
+      }
+      break;
+    default:
+      abort();
+  }
   srand(time(NULL));
   
   struct Shape * shape = NULL;
@@ -29,7 +47,8 @@ int add_random_colors(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, F
     {
       struct VertexArray * cva = get_or_add_array(shape, GL_COLOR_ARRAY);
       cva->num_dimensions = num_dimensions;
-      
+      cva->vertexs = realloc(cva->vertexs, sizeof(float)*shape->num_vertexs*cva->num_dimensions);
+
       long i;
       float color[4];
       set_random_color(color);
@@ -37,7 +56,7 @@ int add_random_colors(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, F
       for (i = 0 ; i < shape->num_vertexs ; i++)
       {
         if (per_shape_or_vertex == 1) set_random_color(color);
-        set_vertex2(shape, i, get_vertex(shape, 0, i), color);
+        set_vertex(shape, 1, i, color);
       }
     }
     write_shape(pipe_out, shape);
