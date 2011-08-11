05bff9ad4164d6d8a1b63315d97009e7ebe0e800
Kevin Branigan <kbranigan@gmail.com>
2011-08-10 22:23:21 -0400

    added some example code to the pass_through script (which is basically an example program to begin with) for using arguments - makes for a better copy-pasta

--- pass_through.c
+++ pass_through.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+//#include <unistd.h>
 
 #define SCHEME_CREATE_MAIN
 #define SCHEME_ASSERT_STDINOUT_ARE_PIPED
@@ -9,10 +10,26 @@
 
 int pass_through(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, FILE * pipe_err)
 {
+  //char filename[300] = "";
+  //int num_attributes = -1;
+  //int c;
+  //while ((c = getopt(argc, argv, "f:a:")) != -1)
+  //switch (c)
+  //{
+  //  case 'f':
+  //    strncpy(filename, optarg, 300);
+  //    break;
+  //  case 'a':
+  //    num_attributes = atoi(optarg);
+  //    break;
+  //  default:
+  //    abort();
+  //}
+  
   struct Shape * shape = NULL;
   while ((shape = read_shape(pipe_in)))
   {
-    /* manipulate data here if you like */
+    // manipulate data here if you like
     write_shape(pipe_out, shape);
     free_shape(shape);
   }
