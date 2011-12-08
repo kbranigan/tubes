2e66fc06842d95b4c4828976f4ca3f159bbfd7b0
Kevin Branigan <kbranigan@gmail.com>
2011-12-07 23:49:13 -0500

    opps

--- src/read_nextbus.sh
+++ src/read_nextbus.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kill `pidof read_nextbus`
-./bin/read_nextbus -n 0 | ./bin/write_sql -de | mysql -uroot nextbus
-./bin/read_nextbus -n 0 -r '' | ./bin/write_sql -de | mysql -uroot nextbus_null
+./bin/read_nextbus -n 0 | ./bin/write_sql -de | mysql -uroot nextbus &
+./bin/read_nextbus -n 0 -r '' | ./bin/write_sql -de | mysql -uroot nextbus_null &
