8f65e853a2f2c410032649855d93f3e2d52c1701
Kevin Branigan <kbranigan@gmail.com>
2011-12-08 17:26:48 -0500

    cleaned up read_shapefile a bunch, it now supports reading dbf files without vertexes - this allows reading strictly tabular data and joining shapefiles for extended options.

--- src/read_shapefile.c
+++ src/read_shapefile.c
@@ -13,7 +13,7 @@
 
 int read_shapefile(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, FILE * pipe_err)
 {
-  char filename[300] = "";
+  char file_name[1000] = "";
   long row_id = -1;
   long part_id = -1;
   int num_attributes = -1;
@@ -21,46 +21,41 @@ int read_shapefile(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, FILE
   while ((c = getopt(argc, argv, "f:r:p:a:")) != -1)
   switch (c)
   {
-    case 'f':
-      strncpy(filename, optarg, 300);
-      break;
-    case 'r':
-      row_id = atol(optarg);
-      break;
-    case 'p':
-      part_id = atol(optarg);
-      break;
-    case 'a':
-      num_attributes = atoi(optarg);
-      break;
-    default:
-      abort();
+    case 'f': strncpy(file_name, optarg, 300); break;
+    case 'r': row_id = atol(optarg); break;
+    case 'p': part_id = atol(optarg); break;
+    case 'a': num_attributes = atoi(optarg); break;
+    default: abort();
   }
   
-  if (strlen(filename) == 0)
+  if (file_name[0] == 0 && argc == 2 && argv[1] != NULL)
+    strncpy(file_name, argv[1], sizeof(file_name));
+  
+  FILE * fp = file_name[0] == 0 ? pipe_in : fopen(file_name, "r");
+  
+  if (fp == NULL || file_name[0] == 0)
   {
-    fprintf(stderr, "%s: must specify a shapefile using -f [filename.dbf]\n", argv[0]);
-    exit(1);
+    fprintf(pipe_err, "ERROR: Usage: %s -f [file_name]\n", argv[0]);
+    return -1;
   }
   
-  FILE * fp = fopen(filename, "r");
-  if (!fp)
+  DBFHandle dbf = DBFOpen(file_name, "rb");
+  //if (dbf == NULL) { fprintf(stderr, "DBFOpen error (%s.dbf)\n", file_name); exit(1); }
+  
+  SHPHandle shp = SHPOpen(file_name, "rb");
+  //if (shp == NULL) { fprintf(stderr, "SHPOpen error (%s.dbf)\n", file_name); exit(1); }
+  
+  if (dbf == NULL && shp == NULL)
   {
-    fprintf(stderr, "%s: error reading shapefile: %s\n", argv[0], filename);
-    exit(1);
+    fprintf(stderr, "%s: Error, neither dbf or shp file found\n", argv[0]);
+    return EXIT_FAILURE;
   }
   
-  DBFHandle d = DBFOpen(filename, "rb");
-  if (d == NULL) { fprintf(stderr, "DBFOpen error (%s.dbf)\n", filename); exit(1); }
-	
-	SHPHandle h = SHPOpen(filename, "rb");
-  if (h == NULL) { fprintf(stderr, "SHPOpen error (%s.dbf)\n", filename); exit(1); }
-	
-  long nRecordCount = DBFGetRecordCount(d);
-  long nFieldCount = DBFGetFieldCount(d);
+  long nRecordCount = DBFGetRecordCount(dbf);
+  long nFieldCount = DBFGetFieldCount(dbf);
   
-  long t=0;
-  long i;
+  long t = 0;
+  long i, j;
   
   /*int num_strings = nFieldCount;
   char ** strings = malloc(sizeof(char*)*num_strings);
@@ -78,34 +73,38 @@ int read_shapefile(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, FILE
   for (i = 0 ; i < nRecordCount ; i++)
   {
     if (row_id != -1 && row_id != i) continue;
-    SHPObject	*psShape = SHPReadObject(h, i);
-    if (psShape->nSHPType == SHPT_POINT)
+    
+    struct Shape * shape = new_shape();
+    
+    if (num_attributes)
+    for (j = 0 ; j < nFieldCount && (j < num_attributes || num_attributes == -1) ; j++)
     {
-      struct Shape * shape = new_shape();
+      char name[20];
+      char value[200];
+      int value_length;
+      DBFFieldType field_type = DBFGetFieldInfo(dbf, j, name, &value_length, NULL);
+      switch (field_type) {
+        case FTString:
+          snprintf(value, 200, "%s", (char*)DBFReadStringAttribute(dbf, i, j));
+          break;
+        case FTInteger:
+          snprintf(value, 200, "%d", DBFReadIntegerAttribute(dbf, i, j));
+          break;
+        case FTDouble:
+          snprintf(value, 200, "%f", DBFReadDoubleAttribute(dbf, i, j));
+          break;
+      }
+      set_attribute(shape, name, value);
+    }
+    
+    SHPObject * psShape = NULL;
+    if (shp) psShape = SHPReadObject(shp, i);
+    
+    if (shp && psShape->nSHPType == SHPT_POINT)
+    {
+      //struct Shape * shape = new_shape();
       shape->unique_set_id = t++;
       //shape->has_attribute_names = 0;
-      
-      int j;
-      if (num_attributes)
-      for (j = 0 ; j < nFieldCount && (j < num_attributes || num_attributes == -1) ; j++)
-      {
-        char name[20];
-        char value[200];
-        int value_length;
-        DBFFieldType field_type = DBFGetFieldInfo(d, j, name, &value_length, NULL);
-        switch (field_type) {
-          case FTString:
-            snprintf(value, 200, "%s", (char*)DBFReadStringAttribute(d, i, j));
-            break;
-          case FTInteger:
-            snprintf(value, 200, "%d", DBFReadIntegerAttribute(d, i, j));
-            break;
-          case FTDouble:
-            snprintf(value, 200, "%f", DBFReadDoubleAttribute(d, i, j));
-            break;
-        }
-        set_attribute(shape, name, value);
-      }
       
       for (j = 0 ; j < psShape->nVertices ; j++)
       {
@@ -115,7 +114,7 @@ int read_shapefile(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, FILE
       write_shape(pipe_out, shape);
       free_shape(shape);
     }
-    else if (psShape->nSHPType == SHPT_ARC || psShape->nSHPType == SHPT_POLYGON)
+    else if (shp && (psShape->nSHPType == SHPT_ARC || psShape->nSHPType == SHPT_POLYGON))
     {
       //fprintf(stderr, "shape %d has %d parts and %d vertices\n", psShape->nShapeId, psShape->nParts, psShape->nVertices);
       
@@ -133,48 +132,59 @@ int read_shapefile(int argc, char ** argv, FILE * pipe_in, FILE * pipe_out, FILE
           end = psShape->panPartStart[iPart+1];
         }
         
-        struct Shape * shape = new_shape();
-        shape->unique_set_id = t++;
+        struct Shape * temp_shape = new_shape();
+        temp_shape->unique_set_id = t++;
         
-        if (psShape->nSHPType == SHPT_ARC)           shape->gl_type = GL_LINE_STRIP;
-        else if (psShape->nSHPType == SHPT_POLYGON)  shape->gl_type = GL_LINE_LOOP;
+        if (psShape->nSHPType == SHPT_ARC)          temp_shape->gl_type = GL_LINE_STRIP;
+        else if (psShape->nSHPType == SHPT_POLYGON) temp_shape->gl_type = GL_LINE_LOOP;
         
-        for (j = 0 ; j < nFieldCount ; j++)
+        /*for (j = 0 ; j < nFieldCount ; j++)
         {
           char name[20];
           char value[200];
           int value_length;
-          DBFFieldType field_type = DBFGetFieldInfo(d, j, name, &value_length, NULL);
+          DBFFieldType field_type = DBFGetFieldInfo(dbf, j, name, &value_length, NULL);
           switch (field_type) {
             case FTString:
-              snprintf(value, 200, "%s", (char*)DBFReadStringAttribute(d, i, j));
+              snprintf(value, 200, "%s", (char*)DBFReadStringAttribute(dbf, i, j));
               break;
             case FTInteger:
-              snprintf(value, 200, "%d", DBFReadIntegerAttribute(d, i, j));
+              snprintf(value, 200, "%d", DBFReadIntegerAttribute(dbf, i, j));
               break;
             case FTDouble:
-              snprintf(value, 200, "%f", DBFReadDoubleAttribute(d, i, j));
+              snprintf(value, 200, "%f", DBFReadDoubleAttribute(dbf, i, j));
               break;
           }
           set_attribute(shape, name, value);
-        }
+        }*/
+        
+        for (j = 0 ; j < shape->num_attributes ; j++)
+          set_attribute(temp_shape, shape->attributes[j].name, shape->attributes[j].value);
         
         for (j = start ; j < end ; j++)
         {
           float v[2] = { psShape->padfX[j], psShape->padfY[j] };
-          append_vertex(shape, v);
+          append_vertex(temp_shape, v);
         }
-        write_shape(pipe_out, shape);
-        free_shape(shape);
+        write_shape(pipe_out, temp_shape);
+        free_shape(temp_shape);
       }
+      free_shape(shape);
     }
-    else
+    else if (shp)
     {
       fprintf(stderr, "doesn't do nSHPType:%d yet.\n", psShape->nSHPType);
       return 0;
     }
-    SHPDestroyObject(psShape);
+    else if (dbf)
+    {
+      free(shape->vertex_arrays);
+      shape->num_vertex_arrays = 0;
+      write_shape(pipe_out, shape);
+      free_shape(shape);
+    }
+    if (shp) SHPDestroyObject(psShape);
   }
-  DBFClose(d);
-  SHPClose(h);
+  if (dbf) DBFClose(dbf);
+  if (shp) SHPClose(shp);
 }
