01b0271bf0f6269943c8a9367e42a90cac6c6ef4
Kevin Branigan <kevin@branigan.ca>
2012-09-27 10:32:57 -0400

    updated inspector to allow counting occurrences of --column (also can sort and so forth) - added unique which does the same thing but pipes the result out

--- block/src/inspect.c
+++ block/src/inspect.c
@@ -11,25 +11,31 @@ int main(int argc, char ** argv)
   //assert_stdin_or_out_is_piped();
   
   static char column_name[1000] = "";
+  static int sort_that_column = 0;
   static int debug = 0;
+  static int reverse = 0;
   
   int c;
   while (1)
   {
     static struct option long_options[] = {
       {"column", required_argument, 0, 'c'},
+      {"sort", no_argument, 0, 's'},
+      {"reverse", no_argument, 0, 'r'},
       {"debug", no_argument, &debug, 1},
       {0, 0, 0, 0}
     };
     
     int option_index = 0;
-    c = getopt_long(argc, argv, "c:", long_options, &option_index);
+    c = getopt_long(argc, argv, "c:sr", long_options, &option_index);
     if (c == -1) break;
     
     switch (c)
     {
       case 0: break;
       case 'c': strncpy(column_name, optarg, sizeof(column_name)); break;
+      case 's': sort_that_column = 1; break;
+      case 'r': reverse = 1; break;
       default: abort();
     }
   }
@@ -37,46 +43,46 @@ int main(int argc, char ** argv)
   struct Block * block = NULL;
   while ((block = read_block(stdin)))
   {
-    int column_id = get_column_id_by_name(block, column_name);
-    if (column_id != -1)
+    if (column_name[0] != 0)
     {
-      struct Column * column = get_column(block, column_id);
-      int column_offset = get_cell(block, 0, column_id) - get_row(block, 0);
+      int column_id = get_column_id_by_name_or_exit(block, column_name);
       
-      int num_counts = 0;
-      int * counts = NULL;
-      void ** values = NULL;
+      struct Column * column = get_column(block, column_id);
+      
+      struct Block * counts = new_block();
+      counts = add_int32_column(counts, "count");
+      int count_column_id = 0;
+      counts = _add_column(counts, column->type, column->bsize, column_get_name(column));
+      int value_column_id = 1;
       
       int i;
       for (i = 0 ; i < block->num_rows ; i++)
       {
-        void * cell = get_row(block, i) + column_offset;
-        int found = 0;
+        void * cell = get_cell(block, i, column_id);
         
         int j;
-        for (j = 0 ; j < num_counts ; j++)
+        for (j = 0 ; j < counts->num_rows ; j++)
         {
-          //if (column->bsize == 0 && strncmp(cell, (char*)values[j], column->type)==0) { counts[j]++; found = 1; }
-          if (memcmp(cell, values[j], column->bsize)==0) { counts[j]++; found = 1; }
+          if (memcmp(cell, get_cell(counts, j, value_column_id), column->bsize)==0) 
+          {
+            set_cell_from_int32(counts, j, count_column_id, get_cell_as_int32(counts, j, count_column_id) + 1);
+            break;
+          }
         }
         
-        if (found == 0)
+        if (j == counts->num_rows) // not found
         {
-          num_counts++;
-          counts = (int*)realloc(counts, sizeof(int)*num_counts);
-          values = (void**)realloc(values, sizeof(void*)*num_counts);
-          counts[num_counts-1] = 1;
-          values[num_counts-1] = cell;
+          counts = add_row(counts);
+          set_cell_from_int32(counts, j, count_column_id, 1); // initialize count at 1 occurrence
+          memcpy(get_cell(counts, j, value_column_id), cell, column->bsize);
         }
-        //if (strcmp(cell, "Local")!=0) break;
       }
       
-      for (i = 0 ; i < num_counts ; i++)
-      {
-        fprintf(stderr, "%6d %s\n", counts[i], (char*)values[i]);
-      }
+      if (sort_that_column == 1 && column->type == TYPE_INT)
+        counts = sort_block_using_int32_column(counts, count_column_id, reverse);
       
-      free(counts);
+      inspect_block(counts);
+      free_block(counts);
     }
     else
       inspect_block(block);
