ffbb4cb104b9925f85722145b7ab3c066de41b2b
Kevin Branigan <kevin@branigan.ca>
2013-01-22 00:18:10 -0500

    added a few generic functions

---
+++ block/src/functions/inout/multiply.c
@@ -0,0 +1,34 @@
+
+#include "../../block.h"
+
+#include <float.h>
+
+struct Block * multiply(struct Block * block, struct Block * opt)
+{
+  const char * column_name = get_attribute_value_as_string(opt, "column_name");
+  double multiple          = get_attribute_value_as_double(opt, "multiple");
+  
+  int column_id = get_column_id_by_name_or_exit(block, (char*)column_name);
+  if (column_id == -1)
+  {
+    fprintf(stderr, "%s() called on '%s' field, wasn't found\n", __func__, column_name);
+    return block;
+  }
+  struct Column * column = get_column(block, column_id);
+  
+  if (column->type != TYPE_FLOAT)
+  {
+    fprintf(stderr, "%s() called on '%s' field, which is of type '%s' (only supports floating point)\n", __func__, column_name, get_type_name(column->type, column->bsize));
+    return block;
+  }
+  
+  char temp[100];
+  snprintf(temp, sizeof(temp), "multiply(column:\"%s\" by %lf)", column_name, multiple);
+  block = add_string_attribute(block, "command", temp);
+  
+  int i;
+  for (i = 0 ; i < block->num_rows ; i++)
+    set_cell_from_double(block, i, column_id, get_cell_as_double(block, i, column_id) * multiple);
+  
+  return block;
+}
