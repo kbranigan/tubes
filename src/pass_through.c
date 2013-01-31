61c03d0dcda87b48fb5e51c87be97472fca5feac
Kevin Branigan <kevin@branigan.ca>
2013-01-30 21:47:18 -0500

    code style changes for src/pass_through.c

--- src/pass_through.c
+++ src/pass_through.c
@@ -1,8 +1,7 @@
 
 #include "block.h"
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char ** argv) {
 	/*
 	if (stdout_is_piped()) // other wise you don't see the seg fault
 		setup_segfault_handling(argv);
@@ -16,8 +15,7 @@ int main(int argc, char ** argv)
 	static int debug = 0;
 	
 	int c;
-	while (1)
-	{
+	while (1) {
 		static struct option long_options[] = {
 			{"filename", required_argument, 0, 'f'},
 			{"header", no_argument, &output_header, 1},
@@ -30,8 +28,7 @@ int main(int argc, char ** argv)
 		c = getopt_long(argc, argv, "d:f:", long_options, &option_index);
 		if (c == -1) break;
 		
-		switch (c)
-		{
+		switch (c) {
 			case 0: break;
 			case 'f': strncpy(filename, optarg, sizeof(filename)); break;
 			default: abort();
@@ -40,26 +37,27 @@ int main(int argc, char ** argv)
 	*/
 	
 	struct Block * block = NULL;
-	while ((block = read_block(stdin)))
-	{
+	while ((block = read_block(stdin))) {
 		/*
 		// foreach shape
 		int shape_start_id = 0, shape_end_id;
-		while ((shape_end_id = get_next_shape_start(block, shape_start_id)))
-		{
+		while ((shape_end_id = get_next_shape_start(block, shape_start_id))) {
 			//int shape_row_id = get_cell_as_int32(block, shape_start_id, shape_row_id_column_id);
 			
 			// foreach part of shape
 			int part_start_id = shape_start_id, part_end_id;
-			while ((part_end_id = get_next_part_start(block, part_start_id)))
-			{
+			while ((part_end_id = get_next_part_start(block, part_start_id))) {
 				//int shape_part_id = get_cell_as_int32(block, shape_start_id, shape_row_id_column_id);
 				
-				if (part_end_id == shape_end_id) break; // last part of shape
+				if (part_end_id == shape_end_id) {
+					break; // last part of shape
+				}
 				part_start_id = part_end_id;
 			}
 			
-			if (shape_end_id == block->num_rows) break; // last shape
+			if (shape_end_id == block->num_rows) {
+				break; // last shape
+			}
 			shape_start_id = shape_end_id;
 		}
 		*/
