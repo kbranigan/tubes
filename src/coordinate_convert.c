fdce0b6e5a78f0708c6ba067579b1eff71a4e15f
Kevin Branigan <kevin@branigan.ca>
2013-02-08 04:01:32 -0500

    coordinate_convert ness

--- src/coordinate_convert.c
+++ src/coordinate_convert.c
@@ -44,21 +44,21 @@ int main(int argc, char ** argv)
 	int zoneletter = 0;
 	int zonenumber = 10;
 	int xtm = 1; // 0=utm, 1=mtm
-	char ellipsoid_string[10] = "nad83"; // silly toronto
+	char ellipsoid_string[10] = "nad27"; // silly toronto
 	int ref_meridian = 0;
 	
 	struct ellipsoid e;
-	get_geo_constants(&e, 5, xtm); // set by nad27 or wgs84
-	/*
-	if (strcmp(ellipsoid_string, "nad27") == 0 || strcmp(ellipsoid_string, "NAD27") == 0) e = geo_constants(5, xtm);
-	else if (strcmp(ellipsoid_string, "nad83") == 0 || strcmp(ellipsoid_string, "NAD83") == 0) e = geo_constants(22, xtm);
-	else if (strcmp(ellipsoid_string, "wgs84") == 0 || strcmp(ellipsoid_string, "WGS84") == 0) e = geo_constants(22, xtm);
+	//get_geo_constants(&e, 5, xtm); // set by nad27 or wgs84
+	
+	if (strcmp(ellipsoid_string, "nad27") == 0 || strcmp(ellipsoid_string, "NAD27") == 0) get_geo_constants(&e, 5, xtm);
+	else if (strcmp(ellipsoid_string, "nad83") == 0 || strcmp(ellipsoid_string, "NAD83") == 0) get_geo_constants(&e, 22, xtm);
+	else if (strcmp(ellipsoid_string, "wgs84") == 0 || strcmp(ellipsoid_string, "WGS84") == 0) get_geo_constants(&e, 22, xtm);
 	else
 	{
 		fprintf(stderr, "invalid ellipsoid, second option should be 'nad27' or 'wgs84' (was %s)\n", ellipsoid_string);
 		exit(1);
 	}
-	*/
+	
 	struct Block * block = NULL;
 	while ((block = read_block(stdin)))
 	{
