0ee7c95c49435361cb5a67aaf0d781f4352d93d6
Kevin Branigan <kevin@branigan.ca>
2013-02-19 23:29:29 -0500

    big changes to write_js

--- src/write_js.c
+++ src/write_js.c
@@ -42,41 +42,103 @@ int main(int argc, char ** argv)
 		return -1;
 	}
 	
-	fprintf(fp, "var data = {\n");
-	fprintf(fp, "  \"data\": [\n");
+	fprintf(fp, "var blocks = {\n");
+	fprintf(fp, "  \"blocks\": [\n");
 	
 	struct Block * block = NULL;
 	while ((block = read_block(stdin)))
 	{
-		int shape_row_id_column_id  = get_column_id_by_name(block, "shape_row_id");
-		int shape_part_id_column_id = get_column_id_by_name(block, "shape_part_id");
-		int red_column_id   = get_column_id_by_name(block, "red");
-		int green_column_id = get_column_id_by_name(block, "green");
-		int blue_column_id  = get_column_id_by_name(block, "blue");
-		int alpha_column_id = get_column_id_by_name(block, "alpha");
+		fprintf(fp, "    {\n");
 		
-		int name_column_id  = -1;
-		if (name_column_name[0] != 0) {
-			name_column_id = get_column_id_by_name(block, name_column_name);
-			fprintf(stderr, "name_column_name = %s, name_column_id = %d\n", name_column_name, name_column_id);
+		fprintf(fp, "      \"attributes\": [");
+		int attr_id;
+		for (attr_id = 0 ; attr_id < block->num_attributes ; attr_id++) {
+			struct Attribute * attr = get_attribute(block, attr_id);
+			fprintf(fp, "%s\n        [\"%s\", ", (attr_id == 0 ? "" : ","), attribute_get_name(attr));
+			if (attr->type == TYPE_CHAR) {
+				fprintf(fp, "\"");
+			}
+			fprintf_attribute_value(fp, block, attr_id); // suppose to addslashes, but I don't want to write that code right now
+			if (attr->type == TYPE_CHAR) {
+				fprintf(fp, "\"");
+			}
+			fprintf(fp, "]");
+		}
+		fprintf(fp, "\n      ],\n");
+		fprintf(fp, "      \"columns\": [");
+		int column_id;
+		for (column_id = 0 ; column_id < block->num_columns ; column_id++) {
+			struct Column * column = get_column(block, column_id);
+			fprintf(fp, "%s\n        [\"%s\", \"%s\"]", (column_id == 0 ? "" : ","), column_get_name(column), get_type_name(column->type, column->bsize));
+		}
+		fprintf(fp, "\n      ],\n");
+		
+		int as_simple_shapes = 1; // kbfu
+		if (as_simple_shapes) {
+			fprintf(fp, "      \"shapes\": [");
+			int shape_start_id = 0, shape_end_id;
+			while ((shape_end_id = get_next_shape_start(block, shape_start_id))) {
+				//int shape_row_id = get_cell_as_int32(block, shape_start_id, shape_row_id_column_id);
+				fprintf(fp, "%s\n        [", (shape_start_id==0) ? "" : ",");
+				
+				// foreach part of shape
+				int part_start_id = shape_start_id, part_end_id;
+				while ((part_end_id = get_next_part_start(block, part_start_id))) {
+					//int shape_part_id = get_cell_as_int32(block, shape_start_id, shape_row_id_column_id);
+					fprintf(fp, "%s[", (part_start_id==shape_start_id) ? "" : ",");
+					
+					int i;
+					for (i = part_start_id ; i < part_end_id ; i++) {
+						fprintf(fp, "%s[%.6f,%.6f]", (i==part_start_id) ? "" : ",", get_x(block, i), get_y(block, i));
+					}
+					
+					fprintf(fp, "]");
+					if (part_end_id == shape_end_id) {
+						break; // last part of shape
+					}
+					part_start_id = part_end_id;
+				}
+				
+				fprintf(fp, "]");
+				if (shape_end_id == block->num_rows) {
+					break; // last shape
+				}
+				shape_start_id = shape_end_id;
+			}
+			fprintf(fp, "\n      ]\n");
+		} else {
+			fprintf(fp, "      \"data\": [");
+			int row_id;
+			for (row_id = 0 ; row_id < block->num_rows ; row_id++) {
+				fprintf(fp, "%s\n        [", (row_id==0) ? "" : ",");
+				for (column_id = 0 ; column_id < block->num_columns ; column_id++) {
+					struct Column * column = get_column(block, column_id);
+					if (column_id != 0) {
+						fprintf(fp, ",");
+					}
+					if (column->type == TYPE_CHAR) {
+						fprintf(fp, "\"");
+					}
+					fprintf_cell(fp, block, row_id, column_id);
+					if (column->type == TYPE_CHAR) {
+						fprintf(fp, "\"");
+					}
+				}
+				fprintf(fp, "]");
+			}
+			fprintf(fp, "\n      ]\n");
 		}
 		
+		/*
 		int shape_start_id = 0, shape_end_id;
 		while ((shape_end_id = get_next_shape_start(block, shape_start_id)))
 		{
 			int shape_row_id = get_cell_as_int32(block, shape_start_id, shape_row_id_column_id);
 			
-			int red = 0, green = 0, blue = 0, alpha = 1;
-			if (red_column_id != -1)   red   = 255 * get_cell_as_double(block, shape_start_id, red_column_id);
-			if (green_column_id != -1) green = 255 * get_cell_as_double(block, shape_start_id, green_column_id);
-			if (blue_column_id != -1)  blue  = 255 * get_cell_as_double(block, shape_start_id, blue_column_id);
-			if (alpha_column_id != -1) alpha = 255 * get_cell_as_double(block, shape_start_id, alpha_column_id) * 0.5;
-			
-			fprintf(fp, "    {\n");
 			if (name_column_id != -1) {
 				fprintf(fp, "      \"name\":\"%s\",\n", (char*)get_cell(block, shape_start_id, name_column_id));
 			}
-			fprintf(fp, "      \"parts\":[\n");
+			fprintf(fp, "      \"parts\":[");
 			
 			//fprintf(stderr, "shape = %d to %d (#%d)\n", shape_start_id, shape_end_id, shape_row_id);
 			
@@ -85,28 +147,30 @@ int main(int argc, char ** argv)
 			{
 				int shape_part_id = get_cell_as_int32(block, shape_start_id, shape_row_id_column_id);
 				
-				fprintf(fp, "        [");
+				fprintf(fp, "[");
 				int i;
 				for (i = part_start_id ; i < part_end_id ; i++)
 				{
-					fprintf(fp, "%.6f,%.6f%s", get_x(block, i), get_y(block, i), (i != part_end_id-1) ? "," : "");
+					fprintf(fp, "[%.6f,%.6f]%s", get_x(block, i), get_y(block, i), (i != part_end_id-1) ? "," : "");
 					if (i % 10000 == 9999) { fprintf(fp, "\n"); }
 				}
 				
-				fprintf(fp, "]\n");
+				fprintf(fp, "]");
 				//fprintf(stderr, "	part = %d to %d (#%d)\n", part_start_id, part_end_id, shape_part_id);
 				if (part_end_id == shape_end_id) break;
 				fprintf(fp, ",");
 				part_start_id = part_end_id;
 			}
 			
-			fprintf(fp, "      ]\n");
-			fprintf(fp, "    }\n");
+			fprintf(fp, "]\n");
+			fprintf(fp, "    }%s", (shape_end_id == block->num_rows) ? "\n" : ",");
 			
 			if (shape_end_id == block->num_rows) break;
-			fprintf(fp, ",");
+			//fprintf(fp, ",");
 			shape_start_id = shape_end_id;
-		}
+		}*/
+		fprintf(fp, "    }\n");
+		free_block(block);
 	}
 	fprintf(fp, "  ]\n");
 	fprintf(fp, "};\n");
