0f1e4d6ab303574d9392a97dfa165f213ba4575d
Kevin Branigan <kevin@branigan.ca>
2013-02-28 00:56:57 -0500

    updated the join_adjacent_geographic_shapes script, works in very basic examples

--- src/join_adjacent_geographic_shapes.c
+++ src/join_adjacent_geographic_shapes.c
@@ -2,6 +2,10 @@
 #include "block.h"
 #include <math.h>
 
+struct Block * each_block(struct Block * block);
+int each_shape(struct Block * block, uint32_t shape_start_id, uint32_t shape_end_id);
+int each_part(struct Block * block, uint32_t part_start_id, uint32_t part_end_id);
+
 /*
 
 The design of this script is _very_ specific.  It takes multiple blocks, and assumes they are all line_loops.
@@ -13,6 +17,92 @@ Also, it compares on red, green and blue, if they exist.
 
 */
 
+struct Block * prev_block = NULL;
+
+struct Block * each_block(struct Block * block) {
+	
+	if (prev_block == NULL) {
+		prev_block = block;
+	} else {
+		foreach_shape(block, &each_shape);
+		write_block(stdout, prev_block);
+		free_block(prev_block);
+	}
+	return block;
+}
+
+int each_shape(struct Block * block, uint32_t shape_start_id, uint32_t shape_end_id) {
+	foreach_part(block, shape_start_id, shape_end_id, &each_part);
+	return 0;
+}
+
+int each_part(struct Block * block, uint32_t part_start_id, uint32_t part_end_id) {
+	if (prev_block == NULL || prev_block == block) return 0;
+	
+	int i, j, i2, j2;
+	for (i = part_start_id ; i < part_end_id-1 ; i++) {
+		int i2 = (i == part_start_id ? part_end_id-1 : i-1);
+		double x1 = get_x(block, i),  y1 = get_y(block, i),
+					 x2 = get_x(block, i2), y2 = get_y(block, i2);
+		
+		// foreach prev_block shape
+		int prev_shape_start_id = 0, prev_shape_end_id;
+		while ((prev_shape_end_id = get_next_shape_start(prev_block, prev_shape_start_id))) {
+			//int shape_row_id = get_cell_as_int32(block, shape_start_id, shape_row_id_column_id);
+			
+			// foreach part of shape
+			int prev_part_start_id = prev_shape_start_id, prev_part_end_id;
+			while ((prev_part_end_id = get_next_part_start(prev_block, prev_part_start_id))) {
+				//int shape_part_id = get_cell_as_int32(block, shape_start_id, shape_row_id_column_id);
+				
+				for (j = prev_part_start_id ; j < prev_part_end_id-1 ; j++) {
+					int j2 = (j == prev_part_end_id-1 ? prev_part_start_id : j+1);
+					double px1 = get_x(prev_block, j),  py1 = get_y(prev_block, j),
+								 px2 = get_x(prev_block, j2), py2 = get_y(prev_block, j2);
+					
+					if ((fabs(x1-px1) < 0.000001 && fabs(y1-py1) < 0.000001) && 
+							(fabs(x2-px2) < 0.000001 && fabs(y2-py2) < 0.000001)) {
+						if (i > i2) {
+							//fprintf(stderr, "haha\n");
+						} else if (j < j2) {
+							
+							fprintf(stderr, "insert block between %d and %d of prev_block\n", j, j2);
+							int old_prev_block_num_rows = prev_block->num_rows;
+							prev_block = set_num_rows(prev_block, prev_block->num_rows + block->num_rows - 2);
+							memmove(get_row(prev_block, j + block->num_rows - 1), get_row(prev_block, j2), prev_block->row_bsize * (old_prev_block_num_rows - j + 1));
+							
+							int32_t shape_row_id_column_id = get_column_id_by_name(prev_block, "shape_row_id");
+							int32_t shape_row_id = get_cell_as_int32(prev_block, j, shape_row_id_column_id);
+							
+							int k;
+							for (k = j2 ; k < j2 + block->num_rows ; k++) {
+								memcpy(get_row(prev_block, k), get_row(block, k-j2), prev_block->row_bsize);
+								set_cell_from_int32(prev_block, k, shape_row_id_column_id, shape_row_id);
+							}
+							
+						} else {
+							fprintf(stderr, "ERROR: uh oh\n");
+						}
+					}
+				}
+				
+				if (prev_part_end_id == prev_shape_end_id) {
+					break; // last part of shape
+				}
+				prev_part_start_id = prev_part_end_id;
+			}
+			
+			if (prev_shape_end_id == block->num_rows) {
+				break; // last shape
+			}
+			prev_shape_start_id = prev_shape_end_id;
+		}
+		
+	}
+	
+	return 0;
+}
+
 int main(int argc, char ** argv) {
 	
 	if (stdout_is_piped()) // other wise you don't see the seg fault
@@ -22,178 +112,10 @@ int main(int argc, char ** argv) {
 	assert_stdout_is_piped();
 	//assert_stdin_or_out_is_piped();
 	
-	/*
-	static char filename[1000] = "";
-	static int debug = 0;
+	add_command_in_foreach(argc, argv);
 	
-	int c;
-	while (1) {
-		static struct option long_options[] = {
-			{"filename", required_argument, 0, 'f'},
-			{"debug", no_argument, &debug, 1},
-			{0, 0, 0, 0}
-		};
-		
-		int option_index = 0;
-		c = getopt_long(argc, argv, "d:f:", long_options, &option_index);
-		if (c == -1) break;
-		
-		switch (c) {
-			case 0: break;
-			case 'f': strncpy(filename, optarg, sizeof(filename)); break;
-			default: abort();
-		}
-	}
-	*/
+	foreach_block(stdin, NULL, 0, &each_block);
 	
-	struct Block * prev_block = NULL;
-	struct Block * block = NULL;
-	while ((block = read_block(stdin))) {
-		
-		if (prev_block != NULL) {
-			
-			int i, j, k;
-			for (i = 0 ; i < block->num_rows-1 ; i++) {
-				double x1 = get_x(block, i), y1 = get_y(block, i);
-				for (j = 0 ; j < prev_block->num_rows-1 ; j++) {
-					double px1 = get_x(prev_block, j), py1 = get_y(prev_block, j);
-					if ((fabs(x1-px1) < 0.000001 && fabs(y1-py1) < 0.000001)) {
-						
-						double x2 = get_x(block, i+1), y2 = get_y(block, i+1);
-						
-						if (j < prev_block->num_rows-2) {
-							double px2 = get_x(prev_block, j+1), py2 = get_y(prev_block, j+1);
-							if (fabs(x2-px2) < 0.000001 && fabs(y2-py2) < 0.000001) {
-								fprintf(stderr, "a: %d-%d to %d-%d\n", i, i+1, j, j+1);
-							}
-						}
-						
-						if (j > 0) {
-							double px2 = get_x(prev_block, j-1), py2 = get_y(prev_block, j-1);
-							if (fabs(x2-px2) < 0.000001 && fabs(y2-py2) < 0.000001) {
-								int32_t shape_row_id = get_cell_as_int32(block, i, get_column_id_by_name(block, "shape_row_id"));
-								int32_t p_shape_row_id = get_cell_as_int32(prev_block, j, get_column_id_by_name(block, "shape_row_id"));
-								fprintf(stderr, "b: %d-%d (%d) to %d-%d (%d)\n", i, i+1, shape_row_id, j, j-1, p_shape_row_id);
-								k = i+1;
-								while (k != i) {
-									fprintf(stderr, "%d ", k);
-									k++;
-									if (k == block->num_rows) {
-										k = 0;
-									}
-								}
-							}
-						}
-						
-						/*
-						int32_t  ids[2] = { ((i > 0) ? (i-1) : block->num_rows-1),      (i < block->num_rows-2 ? (i+1) : 0) };
-						int32_t pids[2] = { ((j > 0) ? (j-1) : prev_block->num_rows-1), (j < prev_block->num_rows-2 ? (j+1) : 0) };
-						
-						double  nx[2] = { get_x(block, ids[0]), get_x(block, ids[1]) };
-						double  ny[2] = { get_y(block, ids[0]), get_y(block, ids[1]) };
-						
-						double npx[2] = { get_x(prev_block, pids[0]), get_x(prev_block, pids[1]) };
-						double npy[2] = { get_y(prev_block, pids[0]), get_y(prev_block, pids[1]) };
-						
-						//double x2 = get_x(block, i+1), y2 = get_y(block, i+1);
-						//double px2 = get_x(prev_block, j+1), y2 = get_y(prev_block, j+1);
-						
-						if (fabs(nx[0]-npx[0]) < 0.000001 && fabs(ny[0]-npy[0]) < 0.000001) {
-							fprintf(stderr, "%d-%d to %d-%d\n", i, ids[0], j, pids[0]);
-						} else if (fabs(nx[1]-npx[0]) < 0.000001 && fabs(ny[1]-npy[0]) < 0.000001) { // only match 2 of 4 combination, otherwise this'll trigger twice
-							fprintf(stderr, "%d-%d to %d-%d\n", i, ids[1], j, pids[0]);
-						}
-						*/
-					}
-				}
-			}
-			
-			/*
-			int prev_shape_start_id = 0, prev_shape_end_id;
-			while ((prev_shape_end_id = get_next_shape_start(prev_block, prev_shape_start_id))) {
-				//int shape_row_id = get_cell_as_int32(prev_block, shape_start_id, shape_row_id_column_id);
-				//fprintf(stderr, "%d-%d\n", prev_shape_start_id, prev_shape_end_id);
-				
-				// foreach part of shape
-				int prev_part_start_id = prev_shape_start_id, prev_part_end_id;
-				while ((prev_part_end_id = get_next_part_start(prev_block, prev_part_start_id))) {
-					//int shape_part_id = get_cell_as_int32(prev_block, shape_start_id, shape_row_id_column_id);
-					//fprintf(stderr, "  %d-%d\n", prev_part_start_id, prev_part_end_id);
-					
-					////////////////////////////////////////////////////////////////////////////////////
-					
-					int shape_start_id = 0, shape_end_id;
-					while ((shape_end_id = get_next_shape_start(block, shape_start_id))) {
-						//int shape_row_id = get_cell_as_int32(block, shape_start_id, shape_row_id_column_id);
-						//fprintf(stderr, "%d-%d\n", shape_start_id, shape_end_id);
-						
-						// foreach part of shape
-						int part_start_id = shape_start_id, part_end_id;
-						while ((part_end_id = get_next_part_start(block, part_start_id))) {
-							//int shape_part_id = get_cell_as_int32(block, shape_start_id, shape_row_id_column_id);
-							//fprintf(stderr, "  %d-%d\n", part_start_id, part_end_id);
-							
-							////////////////////////////////////////////////////////////////////////////////////
-							
-							int i, j;
-							for (i = prev_part_start_id ; i < prev_part_end_id ; i++) {
-								double px = get_x(prev_block, i), py = get_y(prev_block, i);
-								
-								for (j = part_start_id ; i < part_end_id ; i++) {
-									double x = get_x(block, j), y = get_y(block, j);
-									
-									//fprintf(stderr, "%f %f %f %f\n", px, x, py, y);
-									
-									if (fabs(43.718605-y) < 0.0001) { 
-										fprintf(stderr, ";");
-									}
-									
-									if (fabs(px-x) < 0.001 && fabs(py-y) < 0.001) {
-										fprintf(stderr, ".");
-									}
-									//break;
-								}
-								//break;
-							}
-							
-							////////////////////////////////////////////////////////////////////////////////////
-							
-							if (part_end_id == shape_end_id) {
-								break; // last part of shape
-							}
-							part_start_id = part_end_id;
-						}
-						
-						if (shape_end_id == block->num_rows) {
-							break; // last shape
-						}
-						shape_start_id = shape_end_id;
-					}
-					
-					////////////////////////////////////////////////////////////////////////////////////
-					
-					if (prev_part_end_id == prev_shape_end_id) {
-						break; // last part of prev_shape
-					}
-					prev_part_start_id = prev_part_end_id;
-				}
-				
-				if (prev_shape_end_id == prev_block->num_rows) {
-					break; // last prev_shape
-				}
-				prev_shape_start_id = prev_shape_end_id;
-			}*/
-		} else {
-			
-		}
-		
-		//write_block(stdout, block);
-		prev_block = block;
-	}
-	if (prev_block != NULL) {
-		write_block(stdout, prev_block);
-		free_block(prev_block);
-	}
 }
 
 
