0170507022ae8d63929d2a838c0be15f59371dba
Kevin Branigan <kevin@branigan.ca>
2013-03-08 20:18:36 -0500

    updated read_kml, works great now, code is ugly though

--- src/read_kml.c
+++ src/read_kml.c
@@ -15,8 +15,10 @@ int description_column_id = -1;
 struct Style {
 	char styleUrl[10];
 	char lineStyleColor[10];
+	float lineStyleColor4f[4];
 	int lineStyleWidth;
 	char polyStyleColor[10];
+	float polyStyleColor4f[4];
 	int polyStyleFill;
 	int polyStyleOutline;
 };
@@ -25,7 +27,7 @@ struct Style * styles = NULL;
 int num_styles = 0;
 
 void add_style(xmlTextReaderPtr reader) {
-	if (num_styles == 1) return;
+	//if (num_styles == 1) return;
 	num_styles++;
 	styles = (struct Style *)realloc(styles, sizeof(struct Style)*num_styles);
 	memset(&styles[num_styles-1], 0, sizeof(struct Style));
@@ -33,36 +35,132 @@ void add_style(xmlTextReaderPtr reader) {
 	struct Style * style = &styles[num_styles-1];
 	
 	while (xmlTextReaderMoveToNextAttribute(reader)) {
-		char * attr_name = xmlTextReaderName(reader);
-		char * attr_value = xmlTextReaderValue(reader);
-		
-		if (strcmp(attr_name, "id") == 0) {
-			strncpy(style->styleUrl, attr_value, sizeof(style->styleUrl));
+		if (strcmp(xmlTextReaderName(reader), "id") == 0) {
+			strncpy(style->styleUrl, xmlTextReaderValue(reader), sizeof(style->styleUrl));
 			break;
 		}
 	}
 	xmlTextReaderMoveToElement(reader);
+	
 	//fprintf(stderr, "%d: %s\n", num_styles, style->styleUrl);
+	
+	char tagName[100] = "";
+	char tagValue[100] = "";
 	
 	int ret = 1;
 	while (ret == 1) {
-		const xmlChar * tagName = xmlTextReaderConstName(reader);
-		const xmlChar * tagValue = xmlTextReaderConstValue(reader);
+		const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+		const xmlChar * tempTagValue = xmlTextReaderConstValue(reader);
+		if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+		if (tempTagValue != NULL) strncpy(tagValue, tempTagValue, sizeof(tagValue));
 		if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT && xmlTextReaderDepth(reader) == 2 && strcmp(tagName, "Style")==0) {
-			return;
+			break;
 		} else if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT && xmlTextReaderDepth(reader) == 3 && strcmp(tagName, "LineStyle")==0) {
 			while (!(xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT && xmlTextReaderDepth(reader) == 3 && strcmp(tagName, "LineStyle")==0)) {
-				if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_TEXT) {
-					//strncpy(shape_description, xmlTextReaderConstValue(reader), sizeof(shape_description));
+				
+				if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT && xmlTextReaderDepth(reader) == 4 && strcmp(tagName, "color")==0) {
+					while (!(xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT && xmlTextReaderDepth(reader) == 4 && strcmp(tagName, "color")==0)) {
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+							const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+							if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+						}
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_TEXT) {
+							strncpy(style->lineStyleColor, xmlTextReaderConstValue(reader), sizeof(style->lineStyleColor));
+							int red_i = 0, green_i = 0, blue_i = 0, alpha_i = 0;
+							sscanf(style->lineStyleColor, "%2x%2x%2x%2x", &alpha_i, &blue_i, &green_i, &red_i);
+							style->lineStyleColor4f[0] = red_i / 255.0;
+							style->lineStyleColor4f[1] = green_i / 255.0;
+							style->lineStyleColor4f[2] = blue_i / 255.0;
+							style->lineStyleColor4f[3] = alpha_i / 255.0;
+						}
+						ret = xmlTextReaderRead(reader);
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT || xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+							const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+							if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+						}
+					}
+				} else if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT && xmlTextReaderDepth(reader) == 4 && strcmp(tagName, "width")==0) {
+					while (!(xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT && xmlTextReaderDepth(reader) == 4 && strcmp(tagName, "width")==0)) {
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+							const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+							if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+						}
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_TEXT) {
+							style->lineStyleWidth = atoi(xmlTextReaderConstValue(reader));
+						}
+						ret = xmlTextReaderRead(reader);
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT || xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+							const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+							if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+						}
+					}
 				}
 				ret = xmlTextReaderRead(reader);
+				if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT || xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+					const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+					if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+				}
 			}
 		} else if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT && xmlTextReaderDepth(reader) == 3 && strcmp(tagName, "PolyStyle")==0) {
 			while (!(xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT && xmlTextReaderDepth(reader) == 3 && strcmp(tagName, "PolyStyle")==0)) {
-				if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_TEXT) {
-					//strncpy(shape_description, xmlTextReaderConstValue(reader), sizeof(shape_description));
+				
+				if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT && xmlTextReaderDepth(reader) == 4 && strcmp(tagName, "color")==0) {
+					while (!(xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT && xmlTextReaderDepth(reader) == 4 && strcmp(tagName, "color")==0)) {
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+							const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+							if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+						}
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_TEXT) {
+							strncpy(style->polyStyleColor, xmlTextReaderConstValue(reader), sizeof(style->polyStyleColor));
+							int red_i = 0, green_i = 0, blue_i = 0, alpha_i = 0;
+							sscanf(style->polyStyleColor, "%2x%2x%2x%2x", &alpha_i, &blue_i, &green_i, &red_i);
+							style->polyStyleColor4f[0] = red_i / 255.0;
+							style->polyStyleColor4f[1] = green_i / 255.0;
+							style->polyStyleColor4f[2] = blue_i / 255.0;
+							style->polyStyleColor4f[3] = alpha_i / 255.0;
+						}
+						ret = xmlTextReaderRead(reader);
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT || xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+							const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+							if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+						}
+					}
+				} else if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT && xmlTextReaderDepth(reader) == 4 && strcmp(tagName, "outline")==0) {
+					while (!(xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT && xmlTextReaderDepth(reader) == 4 && strcmp(tagName, "outline")==0)) {
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+							const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+							if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+						}
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_TEXT) {
+							style->polyStyleOutline = atoi(xmlTextReaderConstValue(reader));
+						}
+						ret = xmlTextReaderRead(reader);
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT || xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+							const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+							if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+						}
+					}
+				} else if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT && xmlTextReaderDepth(reader) == 4 && strcmp(tagName, "fill")==0) {
+					while (!(xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT && xmlTextReaderDepth(reader) == 4 && strcmp(tagName, "fill")==0)) {
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+							const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+							if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+						}
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_TEXT) {
+							style->polyStyleFill = atoi(xmlTextReaderConstValue(reader));
+						}
+						ret = xmlTextReaderRead(reader);
+						if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT || xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+							const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+							if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+						}
+					}
 				}
 				ret = xmlTextReaderRead(reader);
+				if (xmlTextReaderNodeType(reader) == XML_READER_TYPE_ELEMENT || xmlTextReaderNodeType(reader) == XML_READER_TYPE_END_ELEMENT) {
+					const xmlChar * tempTagName = xmlTextReaderConstName(reader);
+					if (tempTagName != NULL) strncpy(tagName, tempTagName, sizeof(tagName));
+				}
 			}
 		}
 		ret = xmlTextReaderRead(reader);
@@ -117,13 +215,22 @@ struct Block * add_placemark(struct Block * block, xmlTextReaderPtr reader) {
 							int row_id = block->num_rows-1;
 							float x, y, z;
 							sscanf(ptr, "%f,%f,%f", &x, &y, &z);
-							set_shape_part(block, row_id, shape_row_id, 5);
+							set_shape_part(block, row_id, shape_row_id, 0, 5);
 							set_xyz(block, row_id, x, y, z);
 							if (name_column_id != -1) {
 								set_cell_from_string(block, row_id, name_column_id, shape_name);
 							}
 							if (style_column_id != -1) {
-								set_cell_from_string(block, row_id, style_column_id, shape_style);
+								if (shape_style != NULL && shape_style[0] == '#') {
+									set_cell_from_string(block, row_id, style_column_id, &shape_style[1]);
+									int i;
+									for (i = 0 ; i < num_styles ; i++) {
+										if (strncmp(styles[i].styleUrl, &shape_style[1], sizeof(styles[i].styleUrl)) == 0) {
+											set_rgba(block, row_id, styles[i].polyStyleColor4f[0], styles[i].polyStyleColor4f[1], styles[i].polyStyleColor4f[2], styles[i].polyStyleColor4f[3]);
+											break;
+										}
+									}
+								}
 							}
 							if (description_column_id != -1) {
 								set_cell_from_string(block, row_id, description_column_id, shape_description);
