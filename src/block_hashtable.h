429afef6b53937c8b348e18eb92bf0210aaaad54
Kevin Branigan <kevin@branigan.ca>
2014-01-30 20:56:27 -0500

    silly c/c++ name mangling

--- src/block_hashtable.h
+++ src/block_hashtable.h
@@ -4,9 +4,17 @@
 
 struct Block;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void * create_hashtable_on_column(struct Block * block, char * column_name);
 int32_t search_hashtable_for_string(void * ht, char * search);
 int32_t get_hashtable_count(void * ht);
 void free_hashtable(void * ht);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
