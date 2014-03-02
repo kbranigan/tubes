4ffbc486ce44fa24e0dba2b855c8545c7c38154d
Kevin Branigan <kbranigan@gmail.com>
2014-03-02 15:33:38 -0500

    setup read_dem to only add colours when style file provided

--- src/read_dem.c
+++ src/read_dem.c
@@ -34,10 +34,10 @@ void get_chars(int count)
 
 int main(int argc, char ** argv)
 {
-  char style_file[300] = "elevation_colors.csv";//src/read_dem_elevation_style_defaults.txt";
+  char style_file[300] = "";//elevation_colors.csv";//src/read_dem_elevation_style_defaults.txt";
   char filename[300] = "";
   float water_level = 75;
-  int set_invalid_data_to_water_level = 1;
+  int set_invalid_data_to_water_level = 0;
   
 	struct Params * params = NULL;
 	params = add_string_param(params, "filename", 'f', filename, 1);
@@ -48,34 +48,37 @@ int main(int argc, char ** argv)
   pFile = fopen(filename, "rb");
   if (pFile == NULL) { fprintf(stderr, "Usage: %s --filename=[file_name]\n", argv[0]); exit(1); }
   
-  FILE * fpColor = fopen(style_file, "r");
-  if (!fpColor) { fprintf(stderr, "Error: %s -c '%s' should be a valid file\n", argv[0], optarg); exit(1); }
-  char line[1000];
-  
-  while (fgets(line, sizeof(line), fpColor) != NULL)
+  if (style_file[0] != 0)
   {
-    num_colors ++;
-    colors = (struct Color *)realloc(colors, sizeof(struct Color)*num_colors);
-    struct Color * color = &colors[num_colors-1];
-    memset(color, 0, sizeof(struct Color));
+    FILE * fpColor = fopen(style_file, "r");
+    if (!fpColor) { fprintf(stderr, "Error: %s -s '%s' should be a valid file\n", argv[0], optarg); exit(1); }
+    char line[1000];
     
-    char * ptr = strtok(line, " ");
-    if (ptr != NULL) color->elevation = atof(ptr);
-    
-    if (ptr != NULL) ptr = strtok(NULL, " ");
-    color->red   = (ptr != NULL) ? atof(ptr) : 1.0;
-    
-    if (ptr != NULL) ptr = strtok(NULL, " ");
-    color->green = (ptr != NULL) ? atof(ptr) : 1.0;
-    
-    if (ptr != NULL) ptr = strtok(NULL, " ");
-    color->blue  = (ptr != NULL) ? atof(ptr) : 1.0;
-    
-    if (ptr != NULL) ptr = strtok(NULL, " ");
-    color->alpha = (ptr != NULL) ? atof(ptr) : 1.0;
+    while (fgets(line, sizeof(line), fpColor) != NULL)
+    {
+      num_colors ++;
+      colors = (struct Color *)realloc(colors, sizeof(struct Color)*num_colors);
+      struct Color * color = &colors[num_colors-1];
+      memset(color, 0, sizeof(struct Color));
+      
+      char * ptr = strtok(line, " ");
+      if (ptr != NULL) color->elevation = atof(ptr);
+      
+      if (ptr != NULL) ptr = strtok(NULL, " ");
+      color->red   = (ptr != NULL) ? atof(ptr) : 1.0;
+      
+      if (ptr != NULL) ptr = strtok(NULL, " ");
+      color->green = (ptr != NULL) ? atof(ptr) : 1.0;
+      
+      if (ptr != NULL) ptr = strtok(NULL, " ");
+      color->blue  = (ptr != NULL) ? atof(ptr) : 1.0;
+      
+      if (ptr != NULL) ptr = strtok(NULL, " ");
+      color->alpha = (ptr != NULL) ? atof(ptr) : 1.0;
+    }
+    fclose(fpColor);
+  	fprintf(stderr, "num_colors = %d in style_file = '%s'\n", num_colors, style_file);
   }
-  fclose(fpColor);
-	fprintf(stderr, "num_colors = %d in style_file = '%s'\n", num_colors, style_file);
   
   fseek (pFile, 0, SEEK_END);
   lSize = ftell (pFile);
@@ -182,12 +185,18 @@ int main(int argc, char ** argv)
   
   struct Block * block = new_block();
 	block = add_string_attribute(block, "shape_type", "points");
-	block = add_int32_column(block, "shape_row_id");
-	block = add_int32_column(block, "shape_part_id");
-	block = add_int32_column(block, "shape_part_type");
+	if (num_colors > 0)
+  {
+    block = add_int32_column(block, "shape_row_id");
+  	block = add_int32_column(block, "shape_part_id");
+  	block = add_int32_column(block, "shape_part_type");
+  }
   
-	block = add_xy_columns(block);
-	block = add_rgba_columns(block);
+	block = add_xyz_columns(block);
+	if (num_colors > 0)
+  {
+    block = add_rgba_columns(block);
+  }
   
 	block = set_num_rows(block, 1201*1201);
   
@@ -210,7 +219,7 @@ int main(int argc, char ** argv)
         }
       }
       
-      if (color1 != NULL && color2 != NULL)
+      if (num_colors > 0 && color1 != NULL && color2 != NULL)
       {
         float c[4] = { ((elevation_data[col_id][row_id] - color1->elevation) / (color2->elevation - color1->elevation) * (color2->red - color1->red))     + color1->red, 
                        ((elevation_data[col_id][row_id] - color1->elevation) / (color2->elevation - color1->elevation) * (color2->green - color1->green)) + color1->green, 
@@ -243,18 +252,21 @@ int main(int argc, char ** argv)
 				set_cell_from_int32(block, col_id*1201 + row_id, 1, 0);
 				set_cell_from_int32(block, col_id*1201 + row_id, 2, 0);
         //set_cell_from_int32(block, col_id*600 + row_id, 3, elevation_data[col_id][row_id]);
-				set_xy(block, col_id*1201 + row_id, v[0], v[1]);
+				set_xyz(block, col_id*1201 + row_id, v[0], v[1], v[2]);
         set_rgba(block, col_id*1201 + row_id, c[0], c[1], c[2], 1);//c[3]);
       }
       else
       {
-        float c[4] = { 1, 0, 0, 1 };
-				set_cell_from_int32(block, col_id*1201 + row_id, 0, col_id*1201 + row_id);
-				set_cell_from_int32(block, col_id*1201 + row_id, 1, 0);
-				set_cell_from_int32(block, col_id*1201 + row_id, 2, 0);
+        if (num_colors > 0)
+        {
+          float c[4] = { 1, 0, 0, 1 };
+  				set_cell_from_int32(block, col_id*1201 + row_id, 0, col_id*1201 + row_id);
+  				set_cell_from_int32(block, col_id*1201 + row_id, 1, 0);
+  				set_cell_from_int32(block, col_id*1201 + row_id, 2, 0);
+          set_rgba(block, col_id*1201 + row_id, c[0], c[1], c[2], c[3]);
+        }
         //set_cell_from_int32(block, col_id*600 + row_id, 3, elevation_data[col_id][row_id]);
-				set_xy(block, col_id*1201 + row_id, v[0], v[1]);
-        set_rgba(block, col_id*1201 + row_id, c[0], c[1], c[2], c[3]);
+				set_xyz(block, col_id*1201 + row_id, v[0], v[1], v[2]);
       }
       
       //if (row_id > 0    && elevation_data[row_id-1] < elevation_data[col_id][row_id]) { c[0] *= 0.8; c[2] *= 0.8; }
